a2a2f243199f0b12081cc6ba5658df35ffbe784e
youjian <youjian@mgtv.com>
2023-12-28 18:46:06 +0800

    Initial Commit

---
+++ Lua/Lua.h
@@ -0,0 +1,12 @@
+//
+//  Lua.h
+//  Lua
+//
+//  Created by youjian on 2023/12/28.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lua : NSObject
+
+@end
---
+++ Lua/Lua.m
@@ -0,0 +1,12 @@
+//
+//  Lua.m
+//  Lua
+//
+//  Created by youjian on 2023/12/28.
+//
+
+#import "Lua.h"
+
+@implementation Lua
+
+@end
